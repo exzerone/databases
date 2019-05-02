cbd455416be17f2289b348081049e86edba4415b
exzerone <chrishuang128@gmail.com>
2019-05-01 20:40:37 -0700

    Created tables in database chat

--- server/schema.sql
+++ server/schema.sql
@@ -1,10 +1,24 @@
+DROP DATABASE chat;
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE users ( id INTEGER PRIMARY KEY, names TEXT);
+
+CREATE TABLE messages ( 
+  id INTEGER PRIMARY KEY, 
+  message_text TEXT NOT NULL, 
+  users_id INTEGER, 
+  foreign key(users_id) references users(id)
 );
+
+CREATE TABLE rooms 
+( id INTEGER PRIMARY KEY, 
+  roomname TEXT NOT NULL, 
+  messages_id INTEGER, 
+  FOREIGN KEY(messages_id) REFERENCES messages(id)
+);
+
 
 /* Create other tables and define schemas for them here! */
 
